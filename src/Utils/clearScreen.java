f8048f760e77dadc950d4a0bb4daf82cd7be1db1
Ryan <ryanguy.rw@gmail.com>
2024-11-09 14:43:29 +0800

    - Added ClearScreen Util

---
+++ src/Utils/clearScreen.java
@@ -0,0 +1,30 @@
+package Utils;
+import java.io.IOException;
+
+public class clearScreen {
+
+    private static void clearWindows() {
+        try {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void clearUnix() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static void clearScreen() {
+        String os = System.getProperty("os.name").toLowerCase();
+
+        if (os.contains("win")) {
+            clearWindows();
+        } else if (os.contains("nix") || os.contains("nux") || os.contains("mac")) {
+            clearUnix();
+        } else {
+            System.out.println("Unsupported operating system: " + os);
+        }
+    }
+}
