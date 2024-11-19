9cd6c00fa36bd136df6a3caebd49c6370d7f3397
Min Hein Htet <steven.caizuliang@gmail.com>
2024-11-20 01:03:29 +0800

    Merging final code

--- src/InputScanner.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-
-public class InputScanner {
-    // Create a static Scanner instance
-	// So that we dont have to keep defining new scanners in different classes
-    public static Scanner sc = new Scanner(System.in);
-}
